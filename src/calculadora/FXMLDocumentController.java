7e21795ffedee77f65b4b39fb4cc8f078b1fed2b
daw <daw@daw-PC18>
2017-02-28 10:15:38 +0100

    28/02/2017 -- 10:15 Calculadora hecha, falta el histórico

--- src/calculadora/FXMLDocumentController.java
+++ src/calculadora/FXMLDocumentController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -19,12 +22,55 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
+    private TextField fxnumero1;
+    @FXML
+    private TextField fxnumero2;
+    @FXML
+    private Button fxsumar;
+    @FXML
+    private Button fxporcentaje;
+    @FXML
+    private Button fxelevar;
+    @FXML
+    private TextField fxresultado;
+    @FXML
+    private TextArea fxhistorico;
+    @FXML
     private Label label;
+    @FXML
+    private Button fxresto;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void sumar(ActionEvent event) {
+        Double num1 = Double.parseDouble(this.fxnumero1.getText());
+        Double num2 = Double.parseDouble(this.fxnumero2.getText());
+        String suma = String.valueOf(num1+num2);
+        this.fxresultado.setText(suma);
+        this.fxhistorico.appendText(suma);
+    }
+    
+    @FXML 
+    private void porcentaje(ActionEvent event) {
+        Double num1 = Double.parseDouble(this.fxnumero1.getText());
+        Double num2 = Double.parseDouble(this.fxnumero2.getText());
+        String porcentaje = String.valueOf(num1*(num2/100));
+        this.fxresultado.setText(porcentaje);
+    }
+    
+    @FXML
+    private void elevar(ActionEvent event) {
+        Double num1 = Double.parseDouble(this.fxnumero1.getText());
+        Double num2 = Double.parseDouble(this.fxnumero2.getText());
+        String elevar = String.valueOf(Math.pow(num1, num2));
+        this.fxresultado.setText(elevar);
+    }
+    
+    @FXML
+    private void resto(ActionEvent event)  {
+       Double num1 = Double.parseDouble(this.fxnumero1.getText());
+       Double num2 = Double.parseDouble(this.fxnumero2.getText());
+       String resto = String.valueOf(num1%num2);
+       this.fxresultado.setText(resto); 
     }
     
     @Override
